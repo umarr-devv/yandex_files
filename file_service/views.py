5e47019397a4466f107b8579d291287ac67beede
umarr <umarr.developer@gmail.com>
2024-09-20 12:13:31 +0600

    add MainPageView view

--- file_service/views.py
+++ file_service/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from django.shortcuts import render, redirect
+from django.views import generic
 
-# Create your views here.
+from file_service.utils import get_access_token, get_yandex_auth_url
+
+
+class MainPageView(generic.TemplateView):
+    default_template_name = 'main.html'
+    after_auth_template_name = 'public_url.html'
+
+    def get(self, request: HttpRequest, *args, **kwargs):
+        if request.COOKIES.get('access_token'):
+            return render(request, self.after_auth_template_name)
+        return render(request, self.default_template_name)
