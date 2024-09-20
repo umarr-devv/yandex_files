5c5e3d8b4568bb13077bc95a6389ce9710446dfd
umarr <umarr.developer@gmail.com>
2024-09-20 15:43:15 +0600

    add method for get files

--- file_service/utils.py
+++ file_service/utils.py
@@ -1,4 +1,5 @@
 import requests
+from requests import Response
 
 from config import settings
 
@@ -16,3 +17,10 @@ def get_access_token(code: str) -> str:
 
 def get_yandex_auth_url():
     return f"{settings.YANDEX_OAUTH_URL}?response_type=code&client_id={settings.CLIENT_ID}&redirect_uri={settings.REDIRECT_URI}"
+
+
+def get_files_by_public_key(public_key: str, access_token: str) -> Response:
+    return requests.get(
+        settings.YANDEX_PUBLIC_API_URL, params={'public_key': public_key},
+        headers={'Authorization': f'OAuth {access_token}'}
+    )
