4379c15745f01f58ab99e902511886731e52bf17
umarr <umarr.developer@gmail.com>
2024-09-20 12:14:06 +0600

    add AuthYandexView and AuthCallbackView

--- file_service/views.py
+++ file_service/views.py
@@ -13,3 +13,18 @@ class MainPageView(generic.TemplateView):
         if request.COOKIES.get('access_token'):
             return render(request, self.after_auth_template_name)
         return render(request, self.default_template_name)
+
+
+class AuthYandexView(generic.View):
+
+    def get(self, request: HttpRequest, *args, **kwargs):
+        return redirect(get_yandex_auth_url())
+
+
+class AuthCallbackView(generic.View):
+    def get(self, request: HttpRequest, *args, **kwargs):
+        access_token = get_access_token(request.GET.get('code'))
+
+        redirect_response = redirect('main_page')
+        redirect_response.set_cookie('access_token', access_token, max_age=60)
+        return redirect_response
